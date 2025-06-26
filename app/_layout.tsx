4993ab3b4691c9c366fbd0dba9122553708f6fa2
m-dkhan <mdzaidkhan628@gmail.com>
2025-06-26 09:12:00 -0400

    Added Sql lite tables for offline support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
-﻿import { View } from "react-native";
+﻿import { useEffect } from "react";
+import { View, Platform } from "react-native";
 import { Slot } from "expo-router";
+import { initDatabase, verifyTables } from "../services/db";
 
 export default function Layout() {
+  useEffect(() => {
+    if (Platform.OS !== "web") {
+      initDatabase();
+      verifyTables();
+    }
+  }, []);
+
   return <View style={{ flex: 1 }}><Slot /></View>;
 }
